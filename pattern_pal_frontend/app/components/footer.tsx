92e2b392c26c0001a5e7b8f5f43923960dbb9667
IwissManalo <altheairishmanalo@gmail.com>
2025-03-07 07:34:38 +0800

    Create footer.tsx

---
+++ pattern_pal_frontend/app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-800 py-4 px-3 shadow-md shadow-t-lg">
+      <p className="text-white text-xs md:text-sm text-right" >
+        Pattern Pal 2025 &copy; All Rights Reserved
+      </p>
+    </footer>
+  );
+}
