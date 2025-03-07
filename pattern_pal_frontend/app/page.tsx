991a5e00019ce232e8e60907e5e9c1c78eab2661
rapvicente <altheairishmanalo@gmail.com>
2025-03-07 15:47:21 +0800

    Update page.tsx

--- pattern_pal_frontend/app/page.tsx
+++ pattern_pal_frontend/app/page.tsx
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <div className="p-6">
-      <h1 className="text-4xl font-bold">Welcome</h1>
-      <p className="mt-4 text-lg">Username here</p>
+    <div className="mt-50 text-center p-6">
+      <h1 className="text-4xl font-bold">🎨 Welcome to PatternPal ✨</h1>
+      <p className="mt-4 text-lg">Username here, We are glad to have you!</p>
+      <p className="text-justify mt-4 text-lg max-w-2xl mx-auto">
+        Your ultimate companion for crafting and creativity! Whether you're a beginner or a seasoned artist, PatternPal helps you create, share, and bring your patterns to life. Transform images into cross-stitch patterns, follow step-by-step guides, and connect with a community of fellow creators.
+        Start your journey today and turn inspiration not just into stitches but also different art works! 🧵💖
+      </p>
     </div>
   );
 }
